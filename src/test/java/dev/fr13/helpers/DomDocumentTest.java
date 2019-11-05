74a717d01f09b3092d9aeefa4f73f51f24a63220
Nick <fr13.dev@gmail.com>
2019-11-05 08:22:31 +0300

    add test for sorting of main config

--- src/test/java/dev/fr13/helpers/DomDocumentTest.java
+++ src/test/java/dev/fr13/helpers/DomDocumentTest.java
@@ -1,20 +1,13 @@
 package dev.fr13.helpers;
 
-import dev.fr13.exeptions.ParserDomException;
 import org.junit.Test;
 
 import java.io.File;
-
-import static org.junit.Assert.*;
 
 public class DomDocumentTest {
 
     @Test(expected = IllegalStateException.class)
     public void getDom() {
-        try {
-            DomDocument.getDom(new File("dummy"));
-        } catch (ParserDomException e) {
-            throw new IllegalStateException(e);
-        }
+        DomDocument.getDom(new File("dummy"));
     }
 }
